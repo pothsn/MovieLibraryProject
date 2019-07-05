ec7199eed3df73a9d577f9cb54022c3256dffb2b
Nate Poths <pothsn@gmail.com>
2019-07-05 09:08:12 -0500

    Created moviesearch.html and styles.css. Starting with a nav bar, began work on making the site look decent.

--- Client/app.js
+++ Client/app.js
@@ -31,6 +31,15 @@
             }
         });
 
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += "responsive";
+        } 
+        else {
+            x.className = "topnav";
+        }
+    }
         
 
 
