11cd0c88666e216b1995194bb55b88c22678fbaa
Nate Poths <pothsn@gmail.com>
2019-07-05 13:04:18 -0500

    Created addMovie function so long lines are not repeated in both get and post calls. Started work on edit call, dropdown slector box created. Need to implement logic to edit selected movie.

--- Client/app.js
+++ Client/app.js
@@ -1,16 +1,17 @@
 (function($){
-	var j = $('#movies');
-		$.ajax({
-	        	url: 'https://localhost:44352/api/movie',
-	        	type: 'get',
-	        	success: function(movies) {
-	        		$.each(movies, function(i, movie) {
-	        			j.append('<li>Title: ' + movie.Title + ', Genre: ' + movie.Genre + ', Director: '  + movie.DirectorName + '</li>');      		
-	        		});
-	        	},
-	        });
+	var m = $('#movies');
 
-    function processForm( e ){
+    function addMovie(movie)
+    {
+        m.append(
+            '<li>Title: ' + movie.Title + 
+            ', Genre: ' + movie.Genre + 
+            ', Director: '  + movie.DirectorName + '</li>');
+    }
+
+
+    // Take input from form and add movie to list
+    function processAddMovieForm( e ){
         var movie = {
         	Title : this["title"].value,
         	Genre : this["genre"].value,
@@ -25,34 +26,49 @@
             data: JSON.stringify(movie),
             success: function( data, textStatus, jQxhr ){
                 $('#response pre').html( data );
-                j.append('<li>Title: ' + movie.Title + ', Genre: ' + movie.Genre + ', Director: '  + movie.DirectorName + '</li>');
+                addMovie(movie);
             },
             error: function( jqXhr, textStatus, errorThrown ){
             }
         });
-
-    function myFunction() {
-        var x = document.getElementById("myTopnav");
-        if (x.className === "topnav") {
-            x.className += "responsive";
-        } 
-        else {
-            x.className = "topnav";
-        }
-    }
-        
-
-
-
-
-
-
-
-
-
-
         e.preventDefault();
     }
 
-    $('#my-form').submit( processForm );
+    // Display all movies in list below form
+    $.ajax({
+        url: 'https://localhost:44352/api/movie',
+        type: 'get',
+        success: function(movies) {
+            $.each(movies, function(i, movie) {
+            addMovie(movie);             
+            });
+        },
+    });
+
+    // Fill movie dropdown with movie objects
+    $(document).ready(function () {
+        $('#bt').click(function () {
+            
+            var url = 'https://localhost:44352/api/movie';
+
+            $.getJSON(url, function (movies) {
+                $.each(movies, function (index, movie) {
+                    // APPEND OR INSERT DATA TO SELECT ELEMENT.
+                    $('#sel').append('<option value="' + movie.Id + '">' + movie.Title + '</option>');
+                });
+            });
+        });
+
+        // Edit selected value
+        $('#sel').change(function () {
+            $('#msg').text('Selected Item: ' + this.options[this.selectedIndex].text);
+        });
+    });
+
+
+    
+
+
+    $('#addMovieForm').submit( processAddMovieForm );
+    // $('#updateMovieForm').submit( processUpdateMovieForm );
 })(jQuery);
