971e3c46e6a8db7dece5a2e1d6528d88c38a42e1
Nate Poths <pothsn@gmail.com>
2019-07-07 12:12:30 -0500

    Made progress on form collection for updating movie based on selection from dropdown. Thusfar unable to achieve functionality.

--- Client/app.js
+++ Client/app.js
@@ -1,3 +1,5 @@
+let currentSelectedMovie;
+
 (function($){
 	var m = $('#movies');
 
@@ -55,20 +57,42 @@
                 $.each(movies, function (index, movie) {
                     // APPEND OR INSERT DATA TO SELECT ELEMENT.
                     $('#sel').append('<option value="' + movie.Id + '">' + movie.Title + '</option>');
+                    // Need to send movie id on somehow
                 });
             });
         });
 
-        // Edit selected value
+        // SHOW SELECTED VALUE.
         $('#sel').change(function () {
             $('#msg').text('Selected Item: ' + this.options[this.selectedIndex].text);
         });
+
+        $('#updateMovieForm').submit( processUpdateMovieForm );
+        function processUpdateMovieForm( e ){
+            alert("test");
+            let movieId = movie.Id;
+            let newTitleInput = $("#newTitleInput").val();
+            let newGenreInput = $("#newGenreInput").val();
+            let newDirectorInput = $("#newDirectorInput").val();
+            $.ajax({
+            type: 'put',
+            // need to append actual movie id at end instead of 1
+            url: 'https://localhost:44352/api/movie/' + movieId,
+            dataType: 'application/json',
+            data:{
+               "Title": newTitleInput,
+               "Genre": newGenreInput,
+               "DirectorName": newDirectorInput,
+            },
+            success: function (data){
+                alert("YAY!");
+                console.log(data);
+            }
+        });
+        }
     });
 
-
-    
-
-
     $('#addMovieForm').submit( processAddMovieForm );
-    // $('#updateMovieForm').submit( processUpdateMovieForm );
+     
+
 })(jQuery);
